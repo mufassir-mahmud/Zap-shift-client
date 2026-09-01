7ead0ee3d3c6eb070be4229d4b6bab29b95c5ee8
Mufassir Mahmud <mufassir389@gmail.com>
2026-09-01 22:40:08 +0600

    Added Registration Form  using React Hook Form

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router';
 const Logo = () => {
     return (
+        <Link to={"/"}>
         <div className='flex items-end'>
             <img className='' src={logo} alt="" />
             <span className='text-3xl  font-bold -ms-2.5   '>ZapShift</span>
         </div>
+        </Link>
+        
     );
 };
 
--- src/Route/Router.jsx
+++ src/Route/Router.jsx
@@ -3,7 +3,11 @@ import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../Pages/Home/Home";
 import Coverage from "../Pages/Coverage/Coverage";
+import Authentication from "../Layouts/Authentication";
+import Login from "../Pages/Auth/Login/Login";
+import Register from "../Pages/Auth/Register/Register";
 export const router = createBrowserRouter([
+
   {
     path: "/",
     Component: RootLayout,
@@ -19,4 +23,18 @@ export const router = createBrowserRouter([
       }
     ]
   },
+  {
+    path: "/",
+    Component: Authentication,
+    children: [
+      {
+        path: 'login',
+        Component: Login
+      },
+      {
+        path: "register",
+        Component: Register
+      }
+    ]
+  }
 ]);
